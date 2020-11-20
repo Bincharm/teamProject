0dd4e04bc16122f732f9a86fdd2465d6677283da
Bincharm <aisynai@mail.ru>
2020-11-20 21:03:19 +0600

    smt

--- src/com/company/ArstanFile.java
+++ src/com/company/ArstanFile.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class ArstanFile {
     public static void main (String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.print("Hello, World!!!");
     }
 }
